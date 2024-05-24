3ebb9d050e6b47df23d5f416d9c7dc85b9de5de4
jesusAG <jesusayus@gmail.com>
2024-05-24 10:35:13 +0200

    Update TodoList.vue

--- src/components/ToDo/TodoList.vue
+++ src/components/ToDo/TodoList.vue
@@ -2,42 +2,39 @@
     <h1>Lista de Tareas</h1>
     <input class="add-task" v-model="newTask" @keyup.enter="addTask" placeholder="Nueva tarea">
     <ul>
-        <TodoItem v-for="(task, index) in tasks" :key="index" :task="task" :deleteTask="() => deleteTask(index)"
+        <TodoItem v-for="(task, index) in tasks" :key="index" :task="task" 
+            :deleteTask="() => deleteTask(index)" 
             :toggleTask="() => toggleTask(index)" />
     </ul>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref, watchEffect } from 'vue';
 import TodoItem from './TodoItem.vue';
 
 const tasks = ref(JSON.parse(localStorage.getItem('tasks')) || []);
 const newTask = ref('');
 
-const addTask = () => {
-    tasks.value.push({ text: newTask.value, completed: false });
-    newTask.value = '';
+watchEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks.value));
+});
+
+const addTask = () => {
+    if(newTask.value != ''){
+        tasks.value.push({ text: newTask.value, completed: false });
+        newTask.value = '';
+    }
 };
 
 const deleteTask = (index) => {
     tasks.value.splice(index, 1);
-    localStorage.setItem('tasks', JSON.stringify(tasks.value));
 };
 
 const toggleTask = (index) => {
     tasks.value[index].completed = !tasks.value[index].completed;
-    localStorage.setItem('tasks', JSON.stringify(tasks.value));
 };
-
-onMounted(() => {
-    tasks.value = JSON.parse(localStorage.getItem('tasks')) || [];
-});
-
-onUnmounted(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks.value));
-});
 </script>
+
 
 <style scoped>
 .add-task {
